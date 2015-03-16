d5288d8ef78b4fcac38c4654d361af703518ae6e
ZhangShuai <845842278@qq.com>
2015-03-16 15:18:34 +0800

    added edit model

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = patterns('',
 	url(r'^$', views.post_list),
 	url(r'^blog/(?P<id>[0-9]+)/$', views.post_detail),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<id>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +12,29 @@ def post_list(request):
 def post_detail(request, id):
 	post = Post.objects.get(pk=id)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog.views.post_detail', id=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, id):
+	post = Post.objects.get(pk=id)
+
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog.views.post_detail', id=post.pk) 
+	else: 
+		form = PostForm(instance=post)
+	return render(request,'blog/post_edit.html', {'form': form})
